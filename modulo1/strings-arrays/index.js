efc3035397316734f836fd10a05fe8c9e0accc72
soraia-aparecida <soraia211norte@gmail.com>
2021-10-06 17:44:03 -0300

    exercicio-de-strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,68 @@
+// Exercícios de interpretação de código
+/* 1.
+a) undefined
+b) null
+c) 11
+d) 
+e) 
+f)
+
+2."SUBI NUM ÔNIBUS EM MIRROCOS" // 27
+*/
+//Exercícios de interpretação de código
+/*1.
+
+let nomeDoUsuario = prompt ('Informe seu nome')
+let emailDoUsuario = prompt ('Informe seu e-mail')
+
+console.log (`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+*/
+
+/*2.
+const minhasComidasFavoritas = ["Lasanha", "Bolo", "Churrasco", "Pudim", "Mousse de Limão"]
+
+console.log (minhasComidasFavoritas)
+let i = 0
+console.log ("Essas são minhas comidas preferidas:")
+console.log (minhasComidasFavoritas[i])
+console.log (minhasComidasFavoritas[i+1])
+console.log (minhasComidasFavoritas[i+2])
+console.log (minhasComidasFavoritas[i+3])
+console.log (minhasComidasFavoritas[i+4])
+
+const comidaPreferidaUsuario = prompt ("Informe sua comida preferida")
+
+console.log ("Comidas prefiridas:")
+console.log (minhasComidasFavoritas[i])
+console.log (comidaPreferidaUsuario)
+console.log (minhasComidasFavoritas[i+2])
+console.log (minhasComidasFavoritas[i+3])
+console.log (minhasComidasFavoritas[i+4])
+*/
+
+/*3.
+const listaDeTarefas = []
+const resposta1 = prompt ("Informe a primeira tarefa que você precisa realizar hoje:")
+const resposta2 = prompt ("Informe a segunda tarefa que você precisa realizar hoje:")
+const resposta3 = prompt ("Informe a terceira tarefas que você precisa realizar hoje:")
+
+listaDeTarefas.push (resposta1, resposta2, resposta3)
+console.log(listaDeTarefas)
+const tarefaRealizada = prompt ("Informe uma tarefa já realizada: 0, 1 ou 2 ")
+const resposta4 = listaDeTarefas.splice(tarefaRealizada, 1)
+console.log (listaDeTarefas)
+*/
+
+/* Desafio
+1.
+const frase = prompt("Digite uma frase:")
+const fraseSeparada = frase.split(" ")
+console.log (fraseSeparada)
+*/
+/*2.
+const lista = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"] 
+const posicao = lista.indexOf("Abacaxi")
+const tamanhoArray = lista.length
+console.log ("O índice da palavra Abacaxi é:", posicao)
+console.log ("O tamanho do array é:,", tamanhoArray)
+*/
