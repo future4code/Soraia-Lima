b6304f11cd5e9bbc09fe149aa662083054c3a401
soraia-aparecida <soraia211norte@gmail.com>
2021-10-18 17:35:02 -0300

    exercicio-05

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -11,13 +11,13 @@ retornaTamanhoArray(array)
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  return array.reverse()
+    return array.reverse()
 }
 retornaArrayInvertido(array)
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    function comparaNumeros(a, b){
+    function comparaNumeros(a, b) {
         return a - b;
     }
     return array.sort(comparaNumeros)
@@ -28,18 +28,23 @@ retornaArrayOrdenado(array)
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
     const novoArray = array.filter((numero) => {
-        return numero % 2 === 0
-})
-return novoArray
+        return (numero % 2 === 0)
+    })
+    return novoArray
 }
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let paresElevadosADois = []
+    for (let num of retornaNumerosPares(array)) {
+        num = num ** 2
+        paresElevadosADois.push(num)
+    }
+    return paresElevadosADois
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+
 }
 
 // EXERCÍCIO 07
@@ -49,7 +54,7 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -59,27 +64,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -89,10 +94,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
