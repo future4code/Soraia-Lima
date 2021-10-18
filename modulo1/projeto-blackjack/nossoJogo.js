b03d5148c7437284e42d477fde68da0b03af4c64
soraia-aparecida <soraia211norte@gmail.com>
2021-10-18 09:01:25 -0300

    corrigir-a-comparacao-e-comentei-o-codigo

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,42 +1,42 @@
 
-let jogadorUsuario = comprarCarta()
-const jogadorUsuario1 = comprarCarta()
-const jogadorUsuario2 = comprarCarta()
-let jogadorComputador = comprarCarta()
-const jogadorComputador1 = comprarCarta()
-const jogadorComputador2 = comprarCarta()
-const valorUsuario = Number(jogadorUsuario1.valor + jogadorUsuario2.valor)
-const valorComputador = Number(jogadorComputador1.valor + jogadorComputador2.valor)
-const continuar = confirm
+// let jogadorUsuario = comprarCarta()
+// const jogadorUsuario1 = comprarCarta()
+// const jogadorUsuario2 = comprarCarta()
+// let jogadorComputador = comprarCarta()
+// const jogadorComputador1 = comprarCarta()
+// const jogadorComputador2 = comprarCarta()
+// const valorUsuario = Number(jogadorUsuario1.valor + jogadorUsuario2.valor)
+// const valorComputador = Number(jogadorComputador1.valor + jogadorComputador2.valor)
+// const continuar = confirm
 
-console.log("Bem-vindo(a) ao jogo de Blackjack!")
+// console.log("Bem-vindo(a) ao jogo de Blackjack!")
 
-if (confirm("Quer iniciar uma nova rodada?")) {
-   jogadorUsuario = comprarCarta()
-   console.log("Usuário - cartas:", jogadorUsuario1.texto, jogadorUsuario2.texto, "- pontuação", valorUsuario)
+// if (confirm("Quer iniciar uma nova rodada?")) {
+//    jogadorUsuario = comprarCarta()
+//    console.log("Usuário - cartas:", jogadorUsuario1.texto, jogadorUsuario2.texto, "- pontuação", valorUsuario)
 
-   jogadorComputador = comprarCarta()
-   console.log("Computador - cartas:", jogadorComputador1.texto, jogadorComputador2.texto, "- pontuação", valorComputador)
+//    jogadorComputador = comprarCarta()
+//    console.log("Computador - cartas:", jogadorComputador1.texto, jogadorComputador2.texto, "- pontuação", valorComputador)
 
-   if ((valorUsuario <= 21) && (valorComputador <= 21)) {
-      if (valorUsuario === valorComputador) {
-         console.log("Empate!")
-      }
-      else if (valorUsuario > valorComputador) {
-         console.log("O usuário ganhou!")
-      }
-      else if (valorUsuario < valorComputador) {
-         console.log("O computador ganhou!")
+//    if ((valorUsuario <= 21) && (valorComputador <= 21)) {
+//       if (valorUsuario === valorComputador) {
+//          console.log("Empate!")
+//       }
+//       else if (valorUsuario > valorComputador) {
+//          console.log("O usuário ganhou!")
+//       }
+//       else if (valorUsuario < valorComputador) {
+//          console.log("O computador ganhou!")
 
-      }
-   }
-   let i = 0
-   while (continuar === true) {
-      (valorUsuario <= 21) && (valorComputador <= 21)
-      i++
-   }
-}
-else {
-   console.log("O jogo acabou.")
-}
-comprarCarta()
+//       }
+//    }
+//    let i = 0
+//    while (continuar === true) {
+//       (valorUsuario <= 21) && (valorComputador <= 21)
+//       i++
+//    }
+// }
+// else {
+//    console.log("O jogo acabou.")
+// }
+// comprarCarta()
