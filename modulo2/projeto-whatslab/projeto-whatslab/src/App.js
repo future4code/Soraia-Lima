abc5c418ef590a1ddc060cf2db16c4c0d3f03044
soraia-aparecida <soraia211norte@gmail.com>
2021-11-07 16:30:14 -0300

    Exercicio do projeto

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import NovaConversa from './components/NovaConversa';
+
+class App extends React.Component {
+  
+  render() {
+
+    return (
+      <div>
+        <NovaConversa
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
