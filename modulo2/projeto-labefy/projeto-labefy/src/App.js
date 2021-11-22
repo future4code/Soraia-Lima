5c51aa7fed1034b29c11a97593d95466c0d3231f
soraia-aparecida <soraia211norte@gmail.com>
2021-11-21 22:05:46 -0300

    Começo da estilização, mas ainda não finalizada

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -3,12 +3,47 @@ import PagCadastro from './components/PagCadastro'
 import PagLista from './components/PagLista'
 import Home from "./components/home";
 import styled from "styled-components";
+import Logo from "./imagens/logo.jpg"
 
-const Botoes = styled.div `
+
+const Cebecalho = styled.div `
 display: flex;
 width: 100%;
-height: 50%;
-background-color: red;
+height: 8vh;
+align-items: center;
+
+img{
+  width:6vw;
+  height:5vh;
+}
+p{
+  margin-right:18vw;
+  margin-left: -10px;
+}
+button{
+  margin-left:10px;
+  border: none;
+  width:200px;
+  height:5vh;
+  background-color: #CD4B87; 
+  cursor: pointer;
+  border-radius: 50px;
+}
+`
+const Container = styled.div`
+width:100%;
+height:100vh;
+
+`
+const Rodape = styled.div`
+position: absolute;
+bottom: 0;
+background-color: pink;
+width:100vw;
+height:10vh;
+text-align: center;
+
+
 `
 
 class App extends React.Component {
@@ -45,17 +80,22 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-          <Botoes>
+      <Container> 
+          <Cebecalho>
+            <img src={Logo}/> <p>Labefy</p>
             <button onClick={this.irParaHome}>Home</button>
             <button onClick={this.irParaLista}>Lista de musicas</button>
             <button onClick={this.irParaCadastro}>Cadastro</button>
-          </Botoes>
+          </Cebecalho>
+          <Rodape>
+            <p>Telefone: (61) 3333-3333</p>
+            <p>E-mail: labefy@gmail.com</p>
+            
+          </Rodape>
     
-        
         {this.mudarDePagina()}
 
-      </div>
+      </Container>
     );
   }
 }
--- modulo2/projeto-labefy/projeto-labefy/src/components/home.js
+++ modulo2/projeto-labefy/projeto-labefy/src/components/home.js
@@ -1,13 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import Imagem1 from '../imagens/imagem1.jpg'
+import Imagem4 from '../imagens/imagem4.jpg'
+
+const Texto = styled.div`
+text-align: center;
+margin-top:7vh;
+font-size:25px;
+
+img{
+  width:25vw;
+  height:30vh;
+  margin: 25px;
+}
+
+`
 
 class Home extends React.Component {
   render(){
     return (
-      <div>
-      <div>Seja bem-vindo(a) ao labefy! </div>
+      <Texto>
+      <h3>Seja bem-vindo(a) ao labefy! </h3>
+
+      <img src={Imagem1}/>
+      <img src={Imagem4}/>
       {/* <button onClick={this.props.irParaLista}>Ir para lista de musicas</button> */}
+
         
-      </div>
+      </Texto>
     );
   }
 }
