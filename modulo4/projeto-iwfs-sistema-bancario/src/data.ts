c8782f18b702d3f831805b85fa658ac6baad1941
soraia-aparecida <soraia211norte@gmail.com>
2022-01-30 21:47:53 -0300

    desafio 5 ok, desafio caminhando

--- modulo4/projeto-iwfs-sistema-bancario/src/data.ts
+++ modulo4/projeto-iwfs-sistema-bancario/src/data.ts
@@ -1,5 +1,5 @@
 export type Transaction = {
-    value: number | string,
+    value: number,
     date: string,
     description: string
 }
@@ -9,30 +9,46 @@ export type User = {
     name: string,
     cpf: string,
     birthDate: string,
-    balance?: number | string
+    balance?: number,
     extract?: Transaction[]
 }
 
-export const Users: User[] = [
+export const users1: User[] = [
     {
         id: 1,
-        name: "Soraia",
-        cpf: "10100100102",
+        name: "Soraia Silva",
+        cpf: "101.001.001-02",
         birthDate: "17/02/1997",
-        balance: 50.00,
-        extract: [{
-            value: 200.00,
-            date: "01/01/2022",
-            description: "Pagar luz"
-        }]
+        balance: 900.00,
+        extract: [
+            {
+                value: 200.00,
+                date: "01/01/2022",
+                description: "Pagamento da conta luz"
+            }
+        ]
 
     },
     {
         id: 2,
-        name: "Douglas",
-        cpf: "10100100103",
+        name: "Douglas Nunes",
+        cpf: "101.001.001-03",
         birthDate: "26/04/1991",
-        balance: 250.10,
+        balance: 1000.10,
         extract: []
+    },
+    {
+        id: 3,
+        name: "Rodrigo Almeida",
+        cpf: "102.001.001-04",
+        birthDate: "08/10/1989",
+        balance: 699.99,
+        extract: [
+            {
+                value: 50.00,
+                date: "25/01/2022",
+                description: "Pagamento da conta luz"
+            }
+        ]
     }
 ]
--- modulo4/projeto-iwfs-sistema-bancario/src/index.ts
+++ modulo4/projeto-iwfs-sistema-bancario/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Response, Request } from "express"
 import { AddressInfo } from "net"
-import { Users } from "./data"
+import { users1 } from "./data"
 
 const app = express()
 
@@ -15,27 +15,41 @@ const server = app.listen(process.env.PORT || 3003, () => {
     }
 })
 
-const users = Users
+const users = users1
 
 app.get("/users", (req: Request, res: Response) => {
-    try {
-        
-    //     const ajustaPreco = (preco: number): string => {
-    //         let valorAjustado: string = preco.toFixed(2).replace('.', ',')
-    //         return "R$ " + valorAjustado
-    //     }
-    // const users2 = users.map((item) =>{
-    //     item.balance = ajustaPreco(item.balance as number)
-    //     return item
-    // })
-            
-       
-        
+    const cpf = req.query.cpf as string
+    const name = req.query.name as string
 
-        res.status(200).send(users)
-        
-    } catch (error) {
-        res.status(400).send()
+    let errorCode: number = 400
+
+    try {
+
+        if (!cpf) {
+            res.status(200).send(users)
+        }
+
+        const returnProduct = users.filter((item) => {
+            if (cpf && name) {
+                return item.cpf === cpf && item.name.toLowerCase() === name.toLowerCase()
+            }
+            else if (cpf) {
+                return item.cpf === cpf
+            }
+        })
+
+        if (returnProduct.length === 0) {
+            errorCode = 404
+            throw new Error("CPF não encontrado");
+        }
+
+        const retornarValor = returnProduct.map((item) => {
+            return `O saldo do(a) ${item.name} é R$ ${item.balance}`
+        })
+        res.status(200).send(retornarValor)
+
+    } catch (error: any) {
+        res.status(errorCode).send({ message: error.message })
     }
 })
 
@@ -48,12 +62,27 @@ app.post("/users", (req: Request, res: Response) => {
 
         if (!token) {
             errorCode = 401
-            throw new Error("Para realizar essa operação é necesário ter token de autorização");
+            throw new Error("Para realizar essa operação é necesário ter token de autorização")
         }
 
         if (!name && !cpf && !birthDate) {
             errorCode = 404
             throw new Error("Para realizar o cadastro de um novo usário, é necessário informar todos os campos")
+        }
+
+        const validarCaracteresCpf = /[a-zA-Z]/
+        const validarFormatacaoCpf = /\d{3}\.\d{3}\.\d{3}\-\d{2}/
+
+        if (validarCaracteresCpf.test(cpf) || !validarFormatacaoCpf.test(cpf)) {
+            errorCode = 401
+            throw new Error("O campo CPF deve conter 11 caracteres, do tipo number, formatação");
+        }
+
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].cpf === cpf) {
+                errorCode = 409
+                throw new Error("CPF já cadastrado no nosso banco de dados")
+            }
         }
 
         const nascimentoStrArr: string[] = birthDate.split('/')
@@ -82,7 +111,131 @@ app.post("/users", (req: Request, res: Response) => {
     }
 })
 
+app.post("/users/accounts", (req: Request, res: Response) => {
+
+    const token = req.headers.authorization
+    const { description, value, cpf } = req.body
+    let dueDate = req.body.dueDate 
+    let errorCode: number = 400
+
+    try {
+
+        if (!token) {
+            errorCode = 404
+            throw new Error("Para realizar essa operação é necesário ter token de autorização")
+        }
+
+        if (!description || !value || !cpf) {
+            errorCode = 404
+            throw new Error("Para realizar um pagamento, é necessário informar description, value, cpf e dueDate")
+        }
+
+        const currentDate = new Date().toLocaleDateString("pt-BR")
+
+        if (!dueDate) {
+            dueDate = currentDate
+        }         
+
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].cpf === cpf) {
+                if (users[i].balance as number >= value) {
+                    users[i].extract?.push({
+                        value,
+                        date: dueDate,
+                        description,
+                    })
+                    res.status(200).send("Pagamento efetuado com sucesso!")
+                } else {
+                    errorCode = 406
+                    throw new Error("Você não possui saldo suficiente para realizar essa operação :(")
+                }
+            }
+        }
+
+    } catch (error: any) {
+        res.status(errorCode).send({ messagem: error.message })
+    }
+})
+
+app.put("/users/:cpf", (req: Request, res: Response) => {
+
+    const { name, valor } = req.body
+    const cpf = req.params.cpf
+    const token = req.headers.authorization
+    let errorCode: number = 400
 
 
+    try {
+        if (!token) {
+            errorCode = 404
+            throw new Error("Para realizar essa operação é necesário ter token de autorização")
+        }
 
+        if (!name || !valor || !cpf) {
+            errorCode = 404
+            throw new Error("Para realizar um novo depósito, é necessário informar todos os campos, cpf, name e valor")
+        }
+
+        let encontraCpf = false
+
+        for (let i = 0; i < users.length; i++) {
+
+            if (users[i].cpf === cpf && users[i].name.toUpperCase() === name.toUpperCase()) {
+                users[i].balance = users[i].balance + valor
+                users[i].extract?.push({
+                    value: valor,
+                    date: new Date().toLocaleDateString("pt-BR"),
+                    description: "Depósito em dinheiro",
+                })
+                encontraCpf = true
+            }
+        }
+        if (!encontraCpf) {
+            errorCode = 404
+            throw new Error("Usuário não encontrado")
+        }
+        res.status(200).send(`Deposito realizado com sucesso!`)
+
+    } catch (error: any) {
+        res.status(errorCode).send({ messagem: error.message })
+    }
+
+})
+
+app.put("/users/balance/:cpf", (req: Request, res: Response) => {
+    const cpf = req.params.cpf
+    const token = req.headers.authorization
+    let extrato
+    let saldo: number | undefined
+    let errorCode:number = 400
+
+    if (!token) {
+        errorCode = 401
+        throw new Error("Para realizar essa operação é necesário ter token de autorização")
+    }
+
+    if (!cpf) {
+        errorCode = 404
+        throw new Error("Para realizar a consulta de saldo, é necessário informar todos o cpf")
+    }
+
+    try {
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].cpf === cpf){
+                   extrato = users[i].extract 
+                   saldo = users[i].balance                  
+            }
+        }
+
+        const retornarSaldo: any = extrato?.map(item => item.value).reduce((anterior, atual) => anterior + atual,0)
+       console.log("retornar saldo" ,retornarSaldo)
+        const saldoAtualizado = saldo as number - retornarSaldo 
+        
+        res.status(200).send({saldo: saldoAtualizado})
+
+    } catch  (error: any) {
+        res.status(errorCode).send({ message: error.message })
+    }
+   
+})
 
