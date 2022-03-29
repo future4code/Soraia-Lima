1ff6c122e12b7192c12a2f0fc1481f91a82431ce
soraia-aparecida <soraia211norte@gmail.com>
2022-03-29 11:25:52 -0300

    exercícios

---
+++ modulo6/complexidade-de-algoritmos/src/index.ts
@@ -0,0 +1,14 @@
+// Exercício 1
+// Não sabemos o valor do input, por isso ficou O(n). 
+
+// Exercicio 2
+// Não sabemos o valor da string, por isso ficou O(n). 
+
+// Exercício 
+// ficou O(1), porque é diretamente um número que recebemos no parâmetro. 
+
+// Exercício 4
+// Como temos um loop, e dentro dele outro loop, por isso ficou  O(n²). 
+
+// Exercício 5
+// 3, 1, 2, 4. 
